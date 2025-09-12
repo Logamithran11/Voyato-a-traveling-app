d7ca94adabd401377d1fb2284a845bf3f13caf31
loga mithran <logamithran111@gmail.com>
2025-09-12 06:56:05 +0000

    Try fixing this error: `Console Error: FirebaseError: Firebase Storage:

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,8 +4,7 @@ import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
-// IMPORTANT: This is a public configuration and is safe to expose.
-// Security is managed by Firebase Security Rules.
+// This is a public configuration. Security is managed by Firebase Security Rules.
 const firebaseConfig = {
   "projectId": "studio-7035004687-ff43d",
   "appId": "1:88350858292:web:7a5d9f0bab5084ad013e61",
@@ -13,7 +12,6 @@ const firebaseConfig = {
   "apiKey": "AIzaSyCX-SDsKffSIPmpWW2qwJKmCsVAF7pD6JY",
   "authDomain": "studio-7035004687-ff43d.firebaseapp.com",
 };
-
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
