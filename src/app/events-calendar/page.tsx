f0c023dd55a4434ba0780616bc8ef213b08a08e6
loga mithran <logamithran111@gmail.com>
2025-09-26 05:48:28 +0000

    add all the state

--- src/app/events-calendar/page.tsx
+++ src/app/events-calendar/page.tsx
@@ -63,7 +63,9 @@ const allEvents = [
     },
 ];
 
-const allStates = ["All States", "Rajasthan", "Nagaland", "Odisha", "Uttar Pradesh", "Maharashtra", "Kerala"];
+const allStates = [
+    "All States", "Andaman and Nicobar Islands", "Andhra Pradesh", "Arunachal Pradesh", "Assam", "Bihar", "Chandigarh", "Chhattisgarh", "Dadra and Nagar Haveli and Daman and Diu", "Delhi", "Goa", "Gujarat", "Haryana", "Himachal Pradesh", "Jammu and Kashmir", "Jharkhand", "Karnataka", "Kerala", "Ladakh", "Lakshadweep", "Madhya Pradesh", "Maharashtra", "Manipur", "Meghalaya", "Mizoram", "Nagaland", "Odisha", "Puducherry", "Punjab", "Rajasthan", "Sikkim", "Tamil Nadu", "Telangana", "Tripura", "Uttar Pradesh", "Uttarakhand", "West Bengal"
+];
 const allMonths = ["All Months", "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
 export default function EventsCalendarPage() {
@@ -219,5 +221,3 @@ export default function EventsCalendarPage() {
         </div>
     );
 }
-
-    
