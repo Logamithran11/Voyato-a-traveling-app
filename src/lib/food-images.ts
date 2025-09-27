64f934d8cd3d2c93149af65735104e8a5ae79527
loga mithran <logamithran111@gmail.com>
2025-09-27 12:20:16 +0000

    this also not food image

--- src/lib/food-images.ts
+++ src/lib/food-images.ts
@@ -1,11 +1,32 @@
-import data from './food-images.json';
+import { PlaceHolderImages, type ImagePlaceholder } from './placeholder-images';
 
 export type FoodItem = {
   name: string;
   type: string;
   rating: number;
-  image: string;
-  imageHint: string;
+  image: ImagePlaceholder;
 };
 
-export const foodItems: FoodItem[] = data.foodItems;
+const foodData = [
+    { name: "Pani Puri", type: "Street Food", rating: 4.5, imageId: "food-1" },
+    { name: "Vada Pav", type: "Street Food", rating: 4.8, imageId: "food-2" },
+    { name: "Fish Thali", type: "Restaurant", rating: 4.6, imageId: "food-3" },
+    { name: "Chicken Biryani", type: "Restaurant", rating: 4.4, imageId: "food-4" },
+    { name: "Masala Dosa", type: "Restaurant", rating: 4.7, imageId: "food-5" },
+    { name: "Chole Bhature", type: "Street Food", rating: 4.6, imageId: "food-6" },
+    { name: "Galouti Kebab", type: "Restaurant", rating: 4.9, imageId: "food-7" },
+    { name: "Jalebi with Rabri", type: "Dessert", rating: 4.8, imageId: "food-8" }
+];
+
+export const foodItems: FoodItem[] = foodData.map(item => {
+    const imagePlaceholder = PlaceHolderImages.find(p => p.id === item.imageId);
+    if (!imagePlaceholder) {
+        throw new Error(`Could not find placeholder image with id: ${item.imageId}`);
+    }
+    return {
+        name: item.name,
+        type: item.type,
+        rating: item.rating,
+        image: imagePlaceholder
+    };
+});
